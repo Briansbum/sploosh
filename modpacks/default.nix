1ff839de77fae0d1d8cc26066ce666f05ab922b4
Briansbum <freestone.alex@gmail.com>
2026-04-27 20:59:33 +0100

    fix pack sha

--- modpacks/default.nix
+++ modpacks/default.nix
@@ -6,7 +6,7 @@
     displayName = "Create Central";
     mcVersion = "1.21.1";
     loader = "neoforge";
-    modpackHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    modpackHash = "sha256-XpBetpr/i/dAOvjFVIFGgEOwzpuhAmz6K5kqM3AZp5M=";
     jvmOpts = "-Xms4096M -Xmx24576M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200";
   };
 
